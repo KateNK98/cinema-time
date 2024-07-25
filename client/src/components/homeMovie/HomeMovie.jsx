3fd076cdf0859c25226b0532b62d7d2d543baebe
KateNK98 <k.koycheva98@gmail.com>
2024-07-25 23:29:37 +0300

    Add more structure html

--- client/src/components/homeMovie/HomeMovie.jsx
+++ client/src/components/homeMovie/HomeMovie.jsx
@@ -1,8 +1,26 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 export default function HomeMovie() {
     return(
         <div className="row">
             <div className="col">
-            <h2>Movie</h2>
+                <h2>Movie</h2>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                    </Card>
+                </div>
             </div>
         </div>
     )
--- client/src/components/homeVideo/VideoHome.jsx
+++ client/src/components/homeVideo/VideoHome.jsx
@@ -6,7 +6,7 @@ export default function VideoHome() {
       <div className="col">
         <h2 className="mb-2">Our most liked animated movie</h2>
         <div>
-          <iframe width="853" height="480" src="https://www.youtube.com/embed/hXzcyx9V0xw" title="Elemental | Official Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe width="853" height="480" src="https://www.youtube.com/embed/hXzcyx9V0xw" title="Elemental | Official Trailer" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
         
       </div>
