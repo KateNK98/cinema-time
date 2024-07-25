6491cff01fea8baa18686c76e2870ec18a9a1aff
KateNK98 <k.koycheva98@gmail.com>
2024-07-25 22:34:02 +0300

    add and remove slider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Header from './components/header/Header.jsx';
+import Slider from './components/slider/Slider.jsx';
 
 function App() {
 
   return (
     <Container fluid>
       <Header />
+
+      <Slider />  
     </Container>
   )
 }
