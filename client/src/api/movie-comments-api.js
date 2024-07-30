08dea4b9abcf27486a5b271dcf63068ff525a94a
KateNK98 <k.koycheva98@gmail.com>
2024-07-31 00:07:38 +0300

    Correct error

--- client/src/api/movie-comments-api.js
+++ client/src/api/movie-comments-api.js
@@ -1,13 +1,13 @@
-import requester from "./requester"
+import * as request from "./requester"
 
 const BASE_URL = 'http://localhost:3030/jsonstore/cinema/movies/';
 
 const buildUrl = (movieId) => `${BASE_URL}/${movieId}/comments`;
 
-const create = (movieId, username, text) => requester.post(buildUrl(movieId), {username, text});
+const create = (movieId, username, text) => request.post(buildUrl(movieId), {username, text});
 
 const getAllMovieComments = async (movieId) => {
-    const result = await requester.get(buildUrl(movieId));
+    const result = await request.get(buildUrl(movieId));
 
     const comments = Object.values(result);
 
