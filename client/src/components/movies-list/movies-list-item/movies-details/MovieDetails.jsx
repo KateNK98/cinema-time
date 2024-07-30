141e9afed9fefb0878a04d082988a8b4228c450c
KateNK98 <k.koycheva98@gmail.com>
2024-07-30 20:18:13 +0300

    Add html for movie details

--- client/src/components/movies-list/movies-list-item/movies-details/MovieDetails.jsx
+++ client/src/components/movies-list/movies-list-item/movies-details/MovieDetails.jsx
@@ -1,7 +1,38 @@
 export default function MovieDetails() {
     return(
-        <>
-        <h2>Movie details</h2>
-        </>
+        <div className="text-center mt-4">
+            <h2>Movie details</h2>
+            <div className="row">
+                <div className="col">
+                    <img className="mb-5" />
+                    <div className="row">
+                        <div className="col">
+                            <h2>title</h2>
+                            <p>Year: &#x2756;Genre: </p>
+                            <p>Rating</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p></p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <h3>Directed by:</h3>
+                            <p></p>
+                        </div>
+                        <div className="col">
+                            <h3>Writing by:</h3>
+                            <p></p>
+                        </div>
+                        <div className="col">
+                            <h3>Main cast:</h3>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
