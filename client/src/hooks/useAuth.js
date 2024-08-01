7fa2f3aedcc4e8eae0d25f37dc948b3f5888f92f
KateNK98 <k.koycheva98@gmail.com>
2024-08-01 21:44:44 +0300

    Remove save password in auth state

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -6,9 +6,9 @@ export const useLogin = () => {
     const {changeAuthState} = useContext(AuthContext);
 
     const loginHandler = async (email, password) => {
-        const result = await login(email, password);
+        const {password: _, authData} = await login(email, password);
         
-        changeAuthState(result);
+        changeAuthState(authData);
 
         return result;
     }
@@ -20,7 +20,9 @@ export const useRegister = () => {
     const {changeAuthState} = useContext(AuthContext);
 
     const registerHandler = async (email, password) => {
-        const result = await register(email, password);
+        const {password: _, ...authData} = await register(email, password);
+
+        changeAuthState(authData);
 
         
         return result;
